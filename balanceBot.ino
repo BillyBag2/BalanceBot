feaeeb1260b61e370dbc589ce08c7ebfd7166e63
BillyBag2 <BillyBag2@users.noreply.github.com>
2017-10-05 19:52:10 +0100

    Remove RC calibration as not required.

--- balanceBot.ino
+++ balanceBot.ino
@@ -13,16 +13,11 @@ void setup() {
  sbus.begin(false);
 }
 
-int forwardx4_max = 0x600;
-int forwardx4_min = 0x200;
-
 void loop() {
   sbus.process();
   
   int forwardx4 = sbus.getChannel(2) ;
-  int forward = (forwardx4 - forwardx4_min)/4;
-  long scalex256  = ((long)(0x800) * (long)(0x100)) / (long)(forwardx4_max - forwardx4_min);
-  forward = (int)((forward * scalex256)/0x100);
+  int forward = forwardx4/4;  
   forward = forward - 0x100;
   bool gear = sbus.getChannel(5) > 0x400;
   
@@ -30,14 +25,6 @@ void loop() {
   {
     if(gear)
     {
-      if(forwardx4 > forwardx4_max)
-      {
-        forwardx4_max = (forwardx4_max + forwardx4)/2;
-      }
-      else if(forwardx4 < forwardx4_min)
-      {
-        forwardx4 = (forwardx4_min + forwardx4)/2;
-      }
       drive.set(0,0);
     }
     else
